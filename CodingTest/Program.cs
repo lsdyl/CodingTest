50158dfa8794145d9107a29632a0afc82bcbb93b
eli <zcnzhu@qq.com>
2021-08-14 23:08:56 +0800

    添加项目文件。

---
+++ CodingTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("主函数接收参数如下：");
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.Write($"参数{i}：");
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
