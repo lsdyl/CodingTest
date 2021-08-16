b85358b2853e60a149c3387f466aa2e55c2fa214
Lsdyl <zcnzhu@outlook.com>
2021-08-16 12:39:10 +0800

    up

--- CodingTest/Program.cs
+++ CodingTest/Program.cs
@@ -9,11 +9,18 @@ namespace CodingTest
             Console.WriteLine("系统时区：" + TimeZoneInfo.Local);
             Console.WriteLine("当前时区系统时间：" + DateTime.Now);
             Console.WriteLine("当前UTC系统时间：" + DateTime.UtcNow);
-            Console.WriteLine("北京时间："+DateTime.UtcNow.AddHours(8));
-            Console.WriteLine("北京日期："+DateTime.UtcNow.AddHours(8).Date);
-            Console.WriteLine("北京当日0:00的UTC时间："+ DateTime.UtcNow.AddHours(8).Date.ToUniversalTime());
-            Console.WriteLine("当日北京0:00的unix："+ DateTime.UtcNow.AddHours(8).Date.ToUniversalTime().ToUnix());
+            Console.WriteLine();
 
+            DateTime dt = new DateTime(2021, 8, 15, 23, 0, 0, kind: DateTimeKind.Utc);
+            Console.WriteLine("设置当前时区为：" + dt.Kind + "时间为：" + dt);
+            Console.WriteLine("北京时间：" + dt.AddHours(8));
+            dt = dt.Date.AddHours(-8);  //返回日期的Kind与DateTime相同，所以此时Date的Kind是UTC
+            //Console.WriteLine("dt时间的时区为：" + dt.Kind.ToString() + "时间为：" + dt.ToUniversalTime());
+            Console.WriteLine();
+            Console.WriteLine("北京当日0:00的UTC时间：" + dt);
+            Console.WriteLine("当日北京0:00的unix：" + dt.ToUnix());
+
+            Console.WriteLine("当日北京0:00的unix对应的UTC时间为：" +(dt.ToUnix()??0).ToDateTime());
             Console.WriteLine("主函数接收参数如下：");
             for (int i = 0; i < args.Length; i++)
             {
@@ -30,26 +37,26 @@ namespace CodingTest
         ///     如果DateTime未赋值，则返回空
         ///     Unix时间戳：是指格林威治时间1970年01月01日00时00分00秒(北京时间1970年01月01日08时00分00秒)起至现在的总秒数。
         /// </summary>
-        /// <param name="dt">UTC时间</param>
+        /// <param name="dt">必须是UTC时间，且Kind是UTC</param>
         /// <returns>unix</returns>
         public static long? ToUnix(this DateTime dt)
         {
             return dt == new DateTime(0001, 01, 01)
                 ? new long?()
-                : (long)(dt - TimeZoneInfo.ConvertTimeFromUtc(new DateTime(1970, 1, 1), TimeZoneInfo.Local))
+                : (long)(dt - TimeZoneInfo.ConvertTimeFromUtc(new DateTime(1970, 1, 1), TimeZoneInfo.Utc))
                 .TotalSeconds;
         }
 
         /// <summary>
-        ///     将unix转换为当前时区时间
+        ///     将unix转换为UTC时间
         /// </summary>
         /// <param name="unix"></param>
-        /// <returns>本地时间</returns>
+        /// <returns>UTC时间</returns>
         public static DateTime? ToDateTime(this long unix)
         {
             return unix == 0
                 ? new DateTime?() :
-                TimeZoneInfo.ConvertTimeFromUtc(new DateTime(1970, 1, 1), TimeZoneInfo.Local).AddSeconds(unix);
+                TimeZoneInfo.ConvertTimeFromUtc(new DateTime(1970, 1, 1), TimeZoneInfo.Utc).AddSeconds(unix);
         }
     }
 }
